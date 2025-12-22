b57d316830fb76a07c77939b3261e3959ccf3da4
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-12-22 13:18:31 +0100

    fix: get computerChoice

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const player = {
     playerOne : "Player One",
     playerTwo : "Player Two",  
 }
+
+//function randomly chooses any value from the array
+function getComputerChoice(){
+
+    const choices = ["rock", "paper", "scissors"];
+   
+    const randomIndex = Math.floor(Math.random()* choices.length);
+
+    let computerChoice;
+
+    return computerChoice = choices[randomIndex] 
+}
+
+console.log(getComputerChoice())
